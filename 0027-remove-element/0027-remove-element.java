578808273a20cdf6b052d46fec9de8fb96518938
aadityaranjanx69-droid <aadityaranjanx69@gmail.com>
2026-09-08 20:22:24 +0530

    Time: 0 ms (100%), Space: 43.6 MB (23.94%) - LeetHub

---
+++ 0027-remove-element/0027-remove-element.java
@@ -0,0 +1,18 @@
+class Solution {
+    public int removeElement(int[] nums, int val) {
+        int left = 0;
+        int right = 0;
+        while(right < nums.length){
+            if(nums[right] != val){
+                nums[left] = nums[right];
+                right++;
+                left++;
+            }
+           else{
+            right++;
+           }
+        }
+        return left;
+    }
+}
+               

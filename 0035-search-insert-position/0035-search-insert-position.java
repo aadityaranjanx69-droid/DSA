6b092cfce7c1200673e0eefcda6f4841ad081245
aadityaranjanx69-droid <aadityaranjanx69@gmail.com>
2026-09-16 11:57:11 +0530

    Time: 0 ms (100%), Space: 44.6 MB (83.04%) - LeetHub

---
+++ 0035-search-insert-position/0035-search-insert-position.java
@@ -0,0 +1,18 @@
+class Solution {
+    public int searchInsert(int[] nums, int target) {
+        int low = 0;
+        int high = nums.length - 1;
+        int ans = -1;
+        while(low <= high){
+            int mid = (low + high) / 2;
+            if(nums[mid] < target){
+                low = mid + 1;
+            }
+            else{
+                 ans = mid;
+                high = mid - 1;
+            }
+        }
+        return low;
+    }
+}
